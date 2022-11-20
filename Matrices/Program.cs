c440948f87eebcc02d351f331d87921dd04dd9bb
RAFAEL AUGUSTO TORRES PAULINO <rtorrespaulino@outlook.com>
2022-11-20 13:12:02 -0400

    Llenar una matrice con el Bucle For

---
+++ Matrices/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numero =new int[3,3];
+
+            for (int fila = 0; fila < 3; fila++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.WriteLine("Ingresar los valores de la Matriz.");
+                    numero[fila, col] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            Console.Clear();
+            for (int fila = 0; fila < 3; fila++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(" " + numero[fila, col]);
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
